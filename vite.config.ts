f2bb7ff8c7f28c8aea88d4a664a66887d86b3ebd
Bartłomiej Sęk <bartlomiej.sek@bitbag.pl>
2026-07-23 10:20:25 +0200

    Update Vite package to v8

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,17 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     reactRouter(),
-    tsconfigPaths(),
   ],
   resolve: {
+    tsconfigPaths: true,
     alias: {
       "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
     },
   },
   optimizeDeps: {
     include: ['yet-another-react-lightbox'],
-  },
-  build: {
-    rollupOptions: {
-      external: ['yet-another/react-lightbox/styles.css'],
-    },
   },
 });
